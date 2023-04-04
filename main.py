b2c4d3d53db34a77474243beccc19e8c8f930817
yongt88@hs.ac.kr <yongt88@hs.ac.kr>
2023-04-04 16:00:36 +0900

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+## app.py
+from flask import Flask, render_template
+app = Flask(__name__)
+
+# 초기 화면
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# 동적 URL 파라미터 받기
+@app.route('/parameter/<string:param1>/<param2>')
+def parameter(param1, param2):
+    return render_template('parameter.html', param1=param1, param2=param2)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
